f4366a9d71bcfaa55b571a0636573b68cb4bf59d
Santiago <santiagomendivil1009@gmail.com>
2024-04-01 15:33:09 -0600

    Boolean operators in python

---
+++ fundamentals/control-flow/boolean_operators.py
@@ -0,0 +1,37 @@
+"""Control flow in Python"""
+
+# Boolean expressions are the logical statements of True or False
+# The boolean operators help us get a boolean expression from comparing two statements
+
+print("Relational Operators: \n")
+
+# Equals
+print("Equals: ==\n")
+print("Compares two statements, variables, etc. Returns true if are equal, otherwise returns false")
+print("4 == 5 = ", 4 == 5, "\n")
+
+# Not equal
+print("Not equal: !=\n")
+print("The opposite as equals, it compares if two statements are different")
+print("4 != 5 = ", 4 != 5, "\n")
+
+# Greater than
+print("Greater that: >")
+print("Compares if the element on the left is bigger thant the one on the right")
+print("5 > 6 = ", 5 > 6, "\n")
+
+# Greater than or equal
+print("Greater than or equal: >=")
+print("Similar to greater than but also returns true if the elements are equal")
+print("7 >= 7 = ", 7 >= 7, "\n")
+
+# Less than
+print("Less than: <")
+print("The opposite as the greater than, it returns true if the element on the left is smaller")
+print("3 < 4 = ", 3 <= 4, "\n")
+
+
+# Less than or equal
+print("Less than or equal: <=")
+print("The opposite as the greater than or equal")
+print("15 <= 14 = ", 15 <= 14, "\n")
