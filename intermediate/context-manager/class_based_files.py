119a69c9ef43d8c204719a235bd2f16c92401f4f
Santiago <santiagomendivil1009@gmail.com>
2024-04-01 21:23:05 -0600

    Added exceptions to the context manager

--- intermediate/context-manager/class_based_files.py
+++ intermediate/context-manager/class_based_files.py
@@ -7,21 +7,25 @@ class PoemFiles:
     """Poem Reader"""
 
     def __init__(self, poem_file, mode):  # We pass the file and the mode as arguments
-        print('Starting up a poem context manager')
+        print(' -- Starting up a poem context manager -- ')
         self.file = poem_file
         self.mode = mode
+        self.opened_poem_file = None
 
     def __enter__(self):
-        print('Opening poem file')
+        print('\tOpening poem file')
 
         # This variable could produce a warning, ignore it, it works without declaring on init
         self.opened_poem_file = open(self.file, self.mode, encoding="utf-8")
         return self.opened_poem_file
 
-    def __exit__(self, *exc):
-        print('Closing poem file')
+    def __exit__(self, exc_type, exc_value, traceback):
+        print(exc_type)
+        print(exc_value)
+        print(traceback)
         self.opened_poem_file.close()  # Remembering to always close the file
 
 
-with PoemFiles('poem.txt', 'w') as open_poem_file:  # Passing the file and the mode
-    open_poem_file.write('Hope is the thing with feathers')  # Actions
+with PoemFiles('poem.txt', 'r') as open_poem_file:  # Passing the file and the mode
+    print(open_poem_file.read())
+    print(" ---- If exceptions raised, listed below ----")  # Actions
